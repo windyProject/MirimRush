3d038da973015e95d37de37647d3bd05c197cdac
inuse918 <s2018s34@e-mirim.hs.kr>
2020-04-27 01:00:13 +0900

    제한시간(타이머) 예제.

---
+++ TimerEx/Assets/Script/CountDown.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDown : MonoBehaviour
+{
+    float LimitTime = 91f;
+    public Text TimeText;
+    void Update()
+    {
+        LimitTime -= Time.deltaTime;
+
+        if (LimitTime >= 0)
+        {
+
+            if (LimitTime >= 60f)
+            {
+                int b = (int)LimitTime - 60;
+                if (b < 10)
+                    TimeText.text = "1 : " + "0" + b;
+                else
+                    TimeText.text = "1 : " + b;
+            }
+            else
+            {
+                int b = (int)LimitTime;
+                //TimeText.text = b.ToString();
+                if (b < 10)
+                    TimeText.text = "0 : 0" + b;
+                else
+                    TimeText.text = "0 : " + b;
+                
+            }
+
+        }
+
+        else
+        {
+            TimeText.text = "시간끝남";
+            return;
+        }
+    }
+}
